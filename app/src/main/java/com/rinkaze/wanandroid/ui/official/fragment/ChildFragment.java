f433a9afb6e23209e4a553ae866a1a10d2816fe7
ZhouQing0609 <851922220@qq.com>
2019-05-22 11:10:14 +0800

    主分支

--- app/src/main/java/com/rinkaze/wanandroid/ui/official/fragment/ChildFragment.java
+++ app/src/main/java/com/rinkaze/wanandroid/ui/official/fragment/ChildFragment.java
@@ -154,17 +154,6 @@ public class ChildFragment extends BaseFragment<ChildView, ChildPresenter> imple
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-        unbinder1 = ButterKnife.bind(this, rootView);
-        return rootView;
-    }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder1.unbind();
-    }
+
 }
